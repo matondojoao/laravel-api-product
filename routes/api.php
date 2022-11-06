82b9d1a5f3293107a84afa25eeb370f53142d16a
Matondo João <97990105+matondojoao@users.noreply.github.com>
2022-11-06 16:15:53 +0100

    Corrigir o namespace do controllador ProductController no arquivo de rotas

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use ProductController;
+use App\Http\Controllers\Api\ProductController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
